48b7b5bd9df28e9b38dba337aa6ff6879dd5d9d9
Abin Paul Zachariah <39844254+AbinPaulZackariah@users.noreply.github.com>
2018-08-28 10:00:03 +0530

    tts can be disabled now

--- tg_bot/modules/tts_ml.py
+++ tg_bot/modules/tts_ml.py
@@ -36,4 +36,4 @@ def tts(bot: Bot, update: Update, args):
     with open("k.mp3", "rb") as speech:
         update.message.reply_voice(speech, quote=False)
 
-dispatcher.add_handler(CommandHandler('ttsml', tts, pass_args=True))
+dispatcher.add_handler(DisableAbleCommandHandler('ttsml', tts, pass_args=True))
