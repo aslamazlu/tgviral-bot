d961017f098e3ca6bbdd3f6982d9e3803ca46ac2
Abin Paul Zachariah <39844254+AbinPaulZackariah@users.noreply.github.com>
2018-08-30 01:26:22 +0530

    Introducing banme command

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -226,7 +226,29 @@ def kickme(bot: Bot, update: Update):
     else:
         update.effective_message.reply_text("Huh? I can't :/")
 
-
+@run_async
+@bot_admin
+@can_restrict
+@loggable
+def banme(bot: Bot, update: Update):
+    user_id = update.effective_message.from_user.id
+    chat = update.effective_chat
+    user = update.effective_user
+    if is_user_admin(update.effective_chat, user_id):
+        update.effective_message.reply_text("I wish I could... but you're an admin.")
+        return
+    res = update.effective_chat.kick_member(user_id)
+    if res:
+        update.effective_message.reply_text("No problem, banned.")
+        log = "<b>{}:</b>" \
+              "\n#BANME" \
+              "\n<b>User:</b> {}" \
+              "\n<b>ID:</b> <code>{}</code>".format(html.escape(chat.title),
+                                                    mention_html(user.id, user.first_name), user_id)
+        return log
+    else:
+        update.effective_message.reply_text("Huh? I can't :/")
+        
 @run_async
 @bot_admin
 @can_restrict
@@ -350,6 +372,7 @@ BAN_HANDLER = CommandHandler("ban", ban, pass_args=True, filters=Filters.group)
 TEMPBAN_HANDLER = CommandHandler(["tban", "tempban"], temp_ban, pass_args=True, filters=Filters.group)
 KICK_HANDLER = CommandHandler("kick", kick, pass_args=True, filters=Filters.group)
 UNBAN_HANDLER = CommandHandler("unban", unban, pass_args=True, filters=Filters.group)
+BANME_HANDLER = DisableAbleCommandHandler( "banme" , banme, filters = Filters.group)
 KICKME_HANDLER = DisableAbleCommandHandler("kickme", kickme, filters=Filters.group)
 RBAN_HANDLER = CommandHandler("rban", rban, pass_args=True, filters=CustomFilters.sudo_filter)
 
@@ -358,4 +381,5 @@ dispatcher.add_handler(TEMPBAN_HANDLER)
 dispatcher.add_handler(KICK_HANDLER)
 dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
+dispatcher.add_handler(BANME_HANDLER)
 dispatcher.add_handler(RBAN_HANDLER)
